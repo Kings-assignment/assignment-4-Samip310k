4c841bbaf375b24ac652f56f32b9e864fccf3cd5
Samip310k <pokharelsamip12@gmail.com>
2026-04-28 20:18:50 +0545

    Assignment completed

--- src/q15.c
+++ src/q15.c
@@ -1,2 +1,66 @@
 // Write a C program that stores student records (name, roll number, and marks) into a 
 // file and then reads and displays all stored records.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    FILE *fptr;
+    struct Student s;
+    int n, i;
+
+
+    fptr = fopen("students.txt", "w");
+    if (fptr == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d\n", i + 1);
+
+        printf("Name: ");
+        scanf("%s", s.name);
+
+        printf("Roll Number: ");
+        scanf("%d", &s.roll);
+
+        printf("Marks: ");
+        scanf("%f", &s.marks);
+
+    
+        fprintf(fptr, "%s %d %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fptr);
+
+    printf("\nRecords saved successfully!\n");
+
+
+    fptr = fopen("students.txt", "r");
+    if (fptr == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    printf("\nStored Student Records:\n");
+
+
+    while (fscanf(fptr, "%s %d %f", s.name, &s.roll, &s.marks) != EOF) {
+        printf("Name: %s\n", s.name);
+        printf("Roll Number: %d\n", s.roll);
+        printf("Marks: %.2f\n\n", s.marks);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
--- src/q2.c
+++ src/q2.c
@@ -1,1 +1,18 @@
 // Write a C program that reverses an array of characters using pointers.
+#include <stdio.h>
+
+int main (){
+char arr[] = "samip";
+
+char *start = arr;
+char *end;
+int length;
+
+while (*(start+length)!= '\0'){
+    length++;
+}
+
+printf("%d", length);
+
+    return 0;
+}
